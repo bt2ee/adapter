d207380b4373305b3c0a822038bdfe19d0cc7dad
chenle <chenle@forchange.tech>
2022-04-14 22:12:50 +0800

    feat: formatter

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-export default function(axios) {
+export default function (axios) {
   axios.interceptors.response.use(null, async error => {
-    if(error.code == 'ECONNABORTED' && error.message.indexOf('timeout')!=-1){
+    if (error.code == 'ECONNABORTED' && error.message.indexOf('timeout') != -1) {
       const config = error.config;
       config.__retryCount = config.__retryCount || 0;
-      if(config.__retryCount >= config.retry) {
+      if (config.__retryCount >= config.retry) {
         return Promise.reject(error);
       }
       config.__retryCount += 1;
-      const delay = new Promise(function(resolve) {
-          setTimeout(function() {
-            resolve();
-          }, config.retryDelay || 1);
+      const delay = new Promise(function (resolve) {
+        setTimeout(function () {
+          resolve();
+        }, config.retryDelay || 1);
       });
-      return delay.then(function() {
+      return delay.then(function () {
         return axios(config);
       });
-    }else{
+    } else {
       return Promise.reject(error);
     }
   })
