796388e0b97c4075f2b871f86af0ffdd03720c5b
chenle <chenle@forchange.tech>
2022-04-14 21:52:51 +0800

    feat: rename

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 export default function(axios) {
   axios.interceptors.response.use(null, async error => {
     if(error.code == 'ECONNABORTED' && error.message.indexOf('timeout')!=-1){
-      var config = error.config;
+      const config = error.config;
       config.__retryCount = config.__retryCount || 0;
       if(config.__retryCount >= config.retry) {
         return Promise.reject(error);
       }
       config.__retryCount += 1;
-      var newHttp = new Promise(function(resolve) {
+      const delay = new Promise(function(resolve) {
           setTimeout(function() {
-            //console.log('resolve');
             resolve();
           }, config.retryDelay || 1);
       });
-      return newHttp.then(function() {
+      return delay.then(function() {
         return axios(config);
       });
     }else{
